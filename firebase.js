691311c0368a8137fa760c4f10addab22de9468f
Queven <trabalhoqueven@gmail.com>
2026-02-18 11:14:02 -0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,11 +3,11 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAT37Au3gOEqT2KzG9eaxE41X_U4mZez4o",
+  apiKey: "AIzaSyBG_BWBTni0Vt8ha65TQvB84Tnk6HObQNs",
   authDomain: "qmap-981c5.firebaseapp.com",
   projectId: "qmap-981c5",
 };
-
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
 export const db = getFirestore(app);
